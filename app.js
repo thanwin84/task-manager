01a48527822d7a44aebe54189595e4c42efc23e3
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-03-13 20:58:47 +0600

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({
     extended: true
   }))
-  app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 // routes
 app.use('/api/v1/tasks', tasksRoute);
 
